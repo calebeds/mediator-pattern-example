2dcb4077c84c727da67f7bbc1b0e37530f5eab76
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-06-12 09:13:08 -0300

    add mediator example

---
+++ src/UserImpl.java
@@ -0,0 +1,16 @@
+public class UserImpl extends User {
+    public UserImpl(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String msg) {
+        System.out.println(this.name + " is sending the message " + msg);
+        this.mediator.sendMessage(msg, this);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println(this.name + " received message " + msg);
+    }
+}
